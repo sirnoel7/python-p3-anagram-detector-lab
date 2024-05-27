5f5e644f11e0a8f388f2d80553e46b65c521be01
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-05-27 18:16:23 +0300

    hwdone

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [candidate for candidate in word_list if sorted(candidate) == sorted(self.word)]
